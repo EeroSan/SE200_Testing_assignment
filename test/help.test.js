52d7772a66a506562e8a404fd0f495dca6461523
eero santala <eero.t.santala@outlook.com>
2024-11-17 19:02:52 +0200

    added function usage

---
+++ test/help.test.js
@@ -0,0 +1,22 @@
+import add from '../src/add.js';
+// import camelCase from '../src/camelCase.js';
+import filter from '../src/filter.js';
+import isObject from '../src/isObject.js';
+import isSymbol from '../src/isSymbol.js';
+import map from '../src/map.js';
+import memoize from '../src/memoize.js';
+import toFinite from '../src/toFinite.js';
+import toNumber from '../src/toNumber.js';
+import toString from '../src/toString.js';
+// import upperFirst from '../src/upperFirst.js';
+import words from '../src/words.js';
+
+import chai from "chai"
+
+const expect = chai.expect
+
+describe('help', function () {
+    it('always true', function () {
+        expect(1).to.equal(1);
+    });
+});
