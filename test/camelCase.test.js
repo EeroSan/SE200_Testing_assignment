53afa0c65d875aced6fb54a6dba4185471650253
Aleksi Iso-Seppälä <aleksi.is@hotmail.com>
2024-12-01 02:52:25 +0200

    added test cases

---
+++ test/camelCase.test.js
@@ -0,0 +1,54 @@
+/**
+ * Test suite for the camelCase function
+ * 
+ * This suite tests the get function from the '../src/camelCase.js' module
+ * It uses the 'chai' library for assertions.
+ * 
+ * The tests cover the following scenarios:
+ *  - converting to camelCase from separated words
+ *  - converting to camelCase from CAPS WORDS
+ *  - converting to camelCase from word-with-separation-lines
+ *  - converting to camelCase from words_with_underscore
+ *  - 
+ */
+
+import {expect} from 'chai';
+import camelCase from '../src/camelCase.js'
+
+describe('camelCase', () => { 
+
+    // tests for edge cases
+    it('should return empty string for empty input', () => {
+        expect(camelCase('')).to.equal('');
+    });
+
+    it('should return single-word-input as one word all lowercase', () => {
+        expect(camelCase('FOO')).to.equal('foo');
+    });
+
+    // tests for working cases
+    it('should return camelCase of separated words', () => {
+        expect(camelCase('Foo Bar')).to.equal('fooBar');
+    });
+
+    it('should return camelCase of CAPS WORDS', () => {
+        expect(camelCase('FOO BAR')).to.equal('fooBar');
+    });
+
+    it('should return camelCase of underscore_words', () => {
+        expect(camelCase('Foo_Bar')).to.equal('fooBar');
+    });
+
+    it('should return camelCase of words with extra lines', () => {
+        expect(camelCase('---Foo-Bar---')).to.equal('fooBar');
+    });
+
+    it('should return camelCase of words with extra underscores', () => {
+        expect(camelCase('___Foo_Bar___')).to.equal('fooBar');
+    });
+
+    it('should return camelCase for mixed spearators', () => {
+        expect(camelCase('foo_bar-test')).to.equal('fooBarTest');
+    });
+
+});
